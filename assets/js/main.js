681426727df5c86259f54e6076db938e2d6ac4bf
Marcella Cardoso <marcella.sc720@gmail.com>
2024-03-07 20:00:55 -0300

    actual

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,7 @@ function toggleSkills(){
     skillsContent[i].className = 'skills_content skills_close'
   }
   if(itemClass === 'skills_content skills_close'){
-    this.parentNode.className = 'skills_content skills_close'
+    this.parentNode.className = 'skills_content skills_open'
   }
 }
 
@@ -53,7 +53,23 @@ skillsHeader.forEach((el) => {
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll ('[data-target]'),
+      tabContents = document.querySelectorAll ('[data-content')
 
+tabs.forEach (tab => {
+  tab.addEventListener ('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove ('qualification_active')
+    })
+    target.classList.add ('qualification_active')
+      
+    tab.forEach(tab => {
+      tab.classList.remove('qualification_active')
+    })
+    tab.classList.add ('qualification_active')
+  })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
